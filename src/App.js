e9ae2c09d5bc325b495b83145d54b72239f8c175
imrankhan <imran.khan@marginedge.com>
2023-03-07 13:01:28 +0600

    adding all the routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Category from './components/Category/Category';
+import SinglePorduct from './components/SingleProduct/SingleProduct';
+import Newsletter from './components/Footer/Newsletter/Newsletter';
+import AppContext from './utils/context';
+
 function App() {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/category/:id" element={<Category />} />
+                <Route path="/product/:id" element={<SinglePorduct />} />
+            </Routes>
+            <Newsletter />
+            <Footer />
+        </BrowserRouter>
+
+    )
 }
 
 export default App;
