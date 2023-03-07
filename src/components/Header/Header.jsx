59b6c87e223ec7476f0aa236309f27c0b98da162
imrankhan <imran.khan@marginedge.com>
2023-03-07 13:43:26 +0600

    adding Header file skeleton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import Search from "./Search/Search";
+import Cart from "../Cart/Cart";
+import { Context } from "../../utils/context";
+
 import "./Header.scss";
 const Header = () => {
-    return <div>Header</div>;
+  return (
+    <header className="main-header">
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+        <div className="center">JSDEVSTORE.</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
